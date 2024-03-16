0e239f7d59270673b480762dc6a6909d34103301
林柏宇 <117905295+William960321@users.noreply.github.com>
2024-03-16 13:10:05 +1100

    [add] rotate reset in setsetpoint

--- src/main/java/frc/robot/subsystems/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -96,6 +96,7 @@ public class ShooterSubsystem extends SubsystemBase {
     if (hasExceedPhysicalLimit(currentSetpoint) != 0) {
       return;
     }
+    rotatePID.reset();
     double rotateDegree = setpoint + rotateDegreeError;
     if (hasExceedPhysicalLimit(rotateDegree) == -1) {
       rotateDegree = RotateShooterConstants.kRotateAngleMin;
@@ -130,7 +131,7 @@ public class ShooterSubsystem extends SubsystemBase {
       double horizonDistance = tagTracking.getHorizontalDistanceByCT();
       double speakerToShooterHeight = RotateShooterConstants.kSpeakerHeight - RotateShooterConstants.kShooterHeight;
       double degree = Math.toDegrees(Math.atan(speakerToShooterHeight / horizonDistance));
-      return degree + 4.0 * horizonDistance / 4.0;
+      return degree + 9.0 * horizonDistance / 3.6;
     }
     return currentDegree;
   }
