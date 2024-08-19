db91d6edaff23574bbf12e62b6cc50543417841a
Hannahjjj97 <hannahjjj97@gmail.com>
2024-08-19 16:38:49 +0800

    [del] all code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,14 +4,27 @@
 
 package frc.robot;
 
-public class RobotContainer {
+import com.pathplanner.lib.auto.AutoBuilder;
 
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public class RobotContainer {
+    private final SendableChooser<Command> autoChooser;
 
     public RobotContainer() {
+        autoChooser = AutoBuilder.buildAutoChooser();
         configureBindings();
 
     }
 
     private void configureBindings() {
+        SmartDashboard.putData("Auto Chooser", autoChooser);
+    }
+
+    public Command getAutonomousCommand(){
+        return autoChooser.getSelected();
     }
 }
