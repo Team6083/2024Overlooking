cdb4ec39ec386481f8568800b20e02d23b8de4f3
Allenanoob <Mr.skater0102@gmail.com>
2024-12-21 14:20:55 +0800

    [mod]constants   kMaxModuleDriveVoltage -> 8.0

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -178,7 +178,7 @@ public final class Constants {
 
         public static final double kWheelDiameterMeters = 0.15;
 
-        public static final double kMaxModuleDriveVoltage = 4.0;
+        public static final double kMaxModuleDriveVoltage = 8.0;
 
         public static final double kDriveClosedLoopRampRate = 0.1;// 1 second 1 unit
         public static final double kTurningClosedLoopRampRate = 0.0;
